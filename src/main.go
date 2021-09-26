67be1eab8755c0bca6e0f718a095644ede0db5c2
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2021-09-26 23:38:46 +0100

    init

---
+++ src/main.go
@@ -0,0 +1,45 @@
+package scrape2pdf
+
+import (
+	"fmt"
+	"log"
+)
+
+func ExampleNewPDFGenerator() {
+	// Create new PDF generator
+	pdfg, err := NewPDFGenerator()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set global options
+	pdfg.Dpi.Set(300)
+	pdfg.Orientation.Set(OrientationLandscape)
+	pdfg.Grayscale.Set(true)
+
+	// Create a new input page from an URL
+	page := NewPage("https://godoc.org/github.com/SebastiaanKlippert/go-wkhtmltopdf")
+
+	// Set options for this page
+	page.FooterRight.Set("[page]")
+	page.FooterFontSize.Set(10)
+	page.Zoom.Set(0.95)
+
+	// Add to document
+	pdfg.AddPage(page)
+
+	// Create PDF document in internal buffer
+	err = pdfg.Create()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write buffer contents to file on disk
+	err = pdfg.WriteFile("./simplesample.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Done")
+	// Output: Done
+}
---
+++ test/test.go
@@ -0,0 +1,14 @@
+package test
+
+/*
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+*/
+
+/*
+func test(t *testing.T){
+	assert.Equal(t, true, )
+}
+*/
